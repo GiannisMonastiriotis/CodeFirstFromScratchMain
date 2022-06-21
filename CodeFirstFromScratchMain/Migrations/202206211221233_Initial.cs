f3548269a84bca054e486ab2ed1ce4b0158034ff
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-21 15:23:13 +0300

    Added DataOnSeed and First Migration

---
+++ CodeFirstFromScratchMain/Migrations/202206211221233_Initial.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeFirstFromScratchMain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 244),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        CaregoryId = c.Int(nullable: false),
+                        Category_CategoryID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Categories", t => t.Category_CategoryID)
+                .Index(t => t.Category_CategoryID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "Category_CategoryID", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "Category_CategoryID" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ CodeFirstFromScratchMain/Migrations/Configuration.cs
@@ -0,0 +1,42 @@
+﻿namespace CodeFirstFromScratchMain.Migrations
+{
+    using CodeFirstFromScratchMain.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstFromScratchMain.DAL.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstFromScratchMain.DAL.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            var products = new List<Product>()
+            {
+                    new Product
+                    {
+                        ID = 1,
+                        Name ="Kit Kat",
+                        Description ="Chocolate Bar",
+                        Price = 3.3M,
+                        Category = new Category()
+                        {
+                            Name="Sweets",
+                            Description = "Full Sugar"
+                        }
+                    }
+            };
+            products.ForEach(p => context.Products.AddOrUpdate(p));
+            context.SaveChanges();
+        }
+    }
+}
