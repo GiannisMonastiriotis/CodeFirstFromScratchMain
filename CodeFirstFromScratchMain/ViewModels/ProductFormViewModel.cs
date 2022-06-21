99d3a5b25f9c70ea929d2444484c01448f96aa59
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-21 16:07:55 +0300

    Added NewMethod and Modified Edit Method On Products Controller. Added View  For ProfuctsForm

---
+++ CodeFirstFromScratchMain/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CodeFirstFromScratchMain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstFromScratchMain.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public Product  Product { get; set; }
+    }
+}
