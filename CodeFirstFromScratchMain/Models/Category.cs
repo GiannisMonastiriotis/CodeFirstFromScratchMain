bb207ba13a247c6d3beabfd88cf56626ad557ae3
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-21 14:59:42 +0300

    Add project files.

---
+++ CodeFirstFromScratchMain/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstFromScratchMain.Models
+{
+    public class Category
+    {
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
---
+++ CodeFirstFromScratchMain/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstFromScratchMain.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enger a Product Name")]
+        [StringLength(244)]
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Category")]
+        public Category Category { get; set; }
+
+        public int CaregoryId { get; set; }
+    }
+}
