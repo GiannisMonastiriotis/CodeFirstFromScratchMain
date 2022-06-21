7ffea1d332633e532430b91a6201465824f8c2ed
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-21 16:28:14 +0300

    Added Save Method

--- CodeFirstFromScratchMain/Controllers/ProductsController.cs
+++ CodeFirstFromScratchMain/Controllers/ProductsController.cs
@@ -92,6 +92,41 @@ namespace CodeFirstFromScratchMain.Controllers
             return View("ProductForm", viewModel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Save(Product product)
+        {   if(product.ID == 0)
+            {
+                db.Products.Add(product);
+            }
+
+            else
+            {
+                var productInDb = db.Products.Single(p => p.ID == product.ID);
+                productInDb.Name = product.Name;
+                productInDb.Description = product.Description;
+                productInDb.Price = product.Price;
+                productInDb.CaregoryId = product.CaregoryId;
+            }
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new ProductFormViewModel
+                {
+                    Product = product,
+                    Categories = db.Categories.ToList()
+                };
+                return View("ProductForm", viewModel);
+            }
+            else
+                {
+                    db.SaveChanges();
+                }
+           
+
+            return RedirectToAction("Index", "Products");
+        }
+
         // POST: Products/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
