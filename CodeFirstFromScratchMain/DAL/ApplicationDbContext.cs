dfaf762031a92b96d2c1dcdd79f401cc291d4063
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-21 15:15:33 +0300

    Added CONNECTION STRING and Class ApplicationDbContext

---
+++ CodeFirstFromScratchMain/DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeFirstFromScratchMain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstFromScratchMain.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
